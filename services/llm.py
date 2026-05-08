aa406cddbccadea3a6aff0e48dcb44867c94b53b
vrund2005 <vrund765patel@gmail.com>
2026-05-08 11:50:14 +0530

    llm issue

--- services/llm.py
+++ services/llm.py
@@ -3,6 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm1 = ChatGroq(model="openai/gpt-oss-120b")
-llm2 = ChatGroq(model="openai/gpt-oss-20b")
-model = ChatGroq(model="llama-3.3-70b-versatile")
+llm1 = ChatGroq(model="llama-3.3-70b-versatile")      # for new_resume_node (heavy task)
+llm2 = ChatGroq(model="llama-3.1-8b-instant")          # for lighter tasks
+model = ChatGroq(model="llama-3.3-70b-versatile")      # for ats, research, questions nodes
+
+# from langchain_groq import ChatGroq
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm1 = ChatGroq(model="openai/gpt-oss-120b")
+# llm2 = ChatGroq(model="openai/gpt-oss-20b")
+# model = ChatGroq(model="llama-3.3-70b-versatile")
